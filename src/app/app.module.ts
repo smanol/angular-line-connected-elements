179afa0cbb32639d2d0c445d26add3811565f2bc
smanol <smanoliadis@gmail.com>
2023-10-15 19:48:08 +0300

    creating draggable element component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DraggableElementComponent } from './draggable-element/draggable-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DraggableElementComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/draggable-element/draggable-element.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draggable-element',
+  templateUrl: './draggable-element.component.html',
+  styleUrls: ['./draggable-element.component.css']
+})
+export class DraggableElementComponent {
+
+}
